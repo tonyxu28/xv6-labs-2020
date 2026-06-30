04bdb6ff520f0c4c66b3e1fdec23dddc33ceede6
xclaptop <24041212648@stu.xidian.edu.cn>
2026-06-30 18:59:58 +0800

    完成了第二章lab

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,15 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+getfreemem(void)
+{
+  struct run *r;
+  uint64 freemem = 0;
+  acquire(&kmem.lock);
+  for(r = kmem.freelist; r; r = r->next)
+    freemem += PGSIZE;
+  release(&kmem.lock);
+  return freemem;
+}
--- kernel/proc.c
+++ kernel/proc.c
@@ -694,3 +694,18 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64
+getnproc(void)
+{
+  struct proc *p;
+  uint64 nproc = 0;
+  for(p = proc; p < &proc[NPROC]; p++) {
+    acquire(&p->lock);
+    if(p->state != UNUSED) {
+      nproc++;
+    }
+    release(&p->lock);
+  }
+  return nproc;
+}
